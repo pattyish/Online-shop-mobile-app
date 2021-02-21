ba2d2193f7881785e4b0c2fc8b278b6b418cf053
pattyish <patrickishimwe10@gmail.com>
2021-02-21 22:49:30 +0200

    Designing Product Card

--- Screens/products/PoductList.js
+++ Screens/products/PoductList.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { TouchableOpacity, View, Dimensions } from "react-native";
 
+import ProductCard from "./ProductCard";
+
 const windowWith = Dimensions.get("window").width;
 
 const ProductList = (props) => {
+  const { item } = props;
   return (
     <TouchableOpacity style={{ width: "50%" }}>
-      <View
-        style={{ width: windowWith / 2, backgroundColor: "gainsboro" }}
-      ></View>
+      <View style={{ width: windowWith / 2, backgroundColor: "gainsboro" }}>
+        <ProductCard {...item} />
+      </View>
     </TouchableOpacity>
   );
 };
--- Screens/products/ProductCard.js
+++ Screens/products/ProductCard.js
@@ -1,6 +1,79 @@
 import React from "react";
-import { StyleSheet, View, Dimensions, Image, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Image,
+  Text,
+  Button,
+} from "react-native";
 
-const ProductCard = () => {};
+const windowWith = Dimensions.get("window").width;
 
+const ProductCard = (props) => {
+  const { name, price, image, countInStock } = props;
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        resizeMode="contain"
+        source={{
+          uri: image
+            ? image
+            : "https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png",
+        }}
+      />
+      <View style={styles.card} />
+      <Text style={styles.title}>
+        {name.length > 15 ? name.substring(0, 15 - 3) + "...." : name}
+      </Text>
+      <Text style={styles.price}>${price}</Text>
+      {countInStock > 0 ? (
+        <View style={{ marginBottom: 60 }}>
+          <Button title={"Add To Cart"} color={"green"} />
+        </View>
+      ) : (
+        <Text style={{ marginTop: 20 }}> Currently Unvailable</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: windowWith / 2 - 20,
+    height: windowWith / 1.7,
+    padding: 10,
+    borderRadius: 18,
+    marginTop: 55,
+    marginBottom: 5,
+    marginLeft: 10,
+    alignItems: "center",
+    elevation: 8,
+    backgroundColor: "white",
+  },
+  image: {
+    width: windowWith / 2 - 20 - 10,
+    height: windowWith / 2 - 20 - 30,
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: -45,
+  },
+  card: {
+    marginBottom: 10,
+    height: windowWith / 2 - 20 - 90,
+    width: windowWith / 2 - 20 - 10,
+    backgroundColor: "transparent",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 14,
+    textAlign: "center",
+  },
+  price: {
+    fontSize: 20,
+    color: "orange",
+    marginTop: 10,
+  },
+});
 export default ProductCard;
