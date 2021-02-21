e13024b4ca9f90ac1c422a3f78043b169c01457a
pattyish <patrickishimwe10@gmail.com>
2021-02-22 00:34:31 +0200

    Designing Header of App

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 // Screens
+import Header from "./Shared/Header";
 import ProductContainer from "./Screens/Products/ProductsContainer";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <ProductContainer />
-      <StatusBar style="auto" />
     </View>
   );
 }
